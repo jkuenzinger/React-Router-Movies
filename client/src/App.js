537ee4d6f0fe64640e56b44c8bd9fd6bea763f0f
jkuenzinger <jkuenzinger228@gmail.com>
2020-01-28 13:25:35 -0800

    made the changes to router on app broke the page but i think im close

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
-
-  return (
+ return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+    <div>
+  <Route exact path='/' render={() => <MovieList/>}/>
+  <Route  path='/movie/:id' component={Movie}/>
+  </div> 
     </div>
   );
 };
